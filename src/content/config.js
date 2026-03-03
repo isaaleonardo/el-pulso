65cd6adc5d00cf8dbab7aedcb7fb7847921f114f
Isaac Leonardo M. <mcisaacleonardo@gmail.com>
2026-03-03 07:16:10 -0400

    feat: Implement centralized author data management using a JSON file

--- src/content/config.js
+++ src/content/config.js
@@ -1,12 +1,13 @@
+import authors from "../data/authors.json";
 import { defineCollection, z } from "astro:content";
+
+const authorNames = Object.keys(authors);
 
 const notas = defineCollection({
   schema: ({ image }) => z.object({
     title: z.string(),
     description: z.string().optional(),
-    author: z.string(),
-    authorRole: z.string().optional(),
-    authorImage: z.union([image(), z.string()]).optional(),
+    author: z.enum(authorNames),
     date: z.string(),
     category: z.string(),
     heroImage: z.union([image(), z.string()]),
